832946fea6b8fcf028e273905f1dbf78d349db79
D-CURT <https://github.com/D-CURT>
2018-11-24 21:27:48 +0300

    Initial commit

---
+++ src/main/java/dao/driver/JdbcManager.java
@@ -0,0 +1,55 @@
+package dao.driver;
+
+import java.sql.*;
+
+public class JdbcManager {
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("*** The driver connection failed ***");
+        }
+    }
+
+    public static Connection connection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres",
+                    "postgres", "WESt1987");
+        } catch (SQLException e) {
+            System.out.println("*** The connection is not available ***");
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection){
+        if (connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("*** The connection is not exist ***");
+            }
+        }
+    }
+
+    /* при работе с транзакциями рекоменд.закрывать resultSet & statement !!!*/
+    public static void closeStatement(Statement statement){
+        if( statement != null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.out.println("*** The statement is not exist ***");
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet){
+        if( resultSet != null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                System.out.println("*** The resultSet is not exist ***");
+            }
+        }
+    }
+}
