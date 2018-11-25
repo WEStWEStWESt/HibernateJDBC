ea809141271179e943d012b63ab10d78f3efe947
D-CURT <https://github.com/D-CURT>
2018-11-25 15:39:36 +0300

    AnswerTableImplementation added.

---
+++ src/main/java/dao/impl/AnswerTableImplementation.java
@@ -0,0 +1,22 @@
+package dao.impl;
+
+import dao.entity.Answers;
+import dao.sections.SqlQuery;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AnswerTableImplementation extends AbstractTableImplementation {
+
+    public int addAnswer(Answers answers, Connection connection) throws SQLException {
+        int result = 0;
+        String answer = answers.getAnswer();
+        if (selectEntity(answer,
+                SqlQuery.SELECT_QUESTION.getSql(),
+                connection) == null){
+            result = insertEntity(answer,
+                    SqlQuery.INSERT_QUESTION.getSql(),
+                    connection);
+        }
+        return result;
+    }
+}
